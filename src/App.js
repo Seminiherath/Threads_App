8d3bdcc06de1eb3df2e61c95c1a0bf2bb1948505
Semini Herath <semiwasanaherath@gmail.com>
2025-08-16 22:59:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Threadform from '..pages/Threadform';
+
+function App() {
+  return (
+    <div className="App">
+     <h3>Threads Application from ballerina </h3>
+     <Threadform/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Threadform.js
@@ -0,0 +1,11 @@
+// pages/CreateThread.jsx
+import CreateThreadForm from '../components/CreateThreadForm';
+
+function CreateThread() {
+  return (
+    <div>
+      <h1>Create a New Thread</h1>
+      <CreateThreadForm />
+    </div>
+  );
+}
